8cd4cf5cd0bbf12be362f731b125f451a338d4a2
Gbogosam <soapscodes@gmail.com>
2023-03-24 13:07:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,65 +4,109 @@ const postsContainer = document.querySelector('.posts');
 
 // Function to create a new post element
 function createPostElement(text) {
-    const postCard = document.createElement('div');
-    postCard.classList.add('post');
-  
-    const postElement = document.createElement('div');
-    postElement.classList.add('post-content');
-  
-    const userPosted = document.createElement('small');
-    userPosted.textContent = "a user posted";
-    userPosted.classList.add('user-posted');
+  const postCard = document.createElement('div');
+  postCard.classList.add('post');
 
-    const postTextElement = document.createElement('p');
-    postTextElement.textContent = text;
-    postTextElement.classList.add('post-text');
+  const postElement = document.createElement('div');
+  postElement.classList.add('post-content');
+
+  const userPosted = document.createElement('small');
+  userPosted.textContent = "a user posted";
+  userPosted.classList.add('user-posted');
+
+  const postTextElement = document.createElement('p');
+  postTextElement.textContent = text;
+  postTextElement.classList.add('post-text');
+
+  const likeButton = document.createElement('button');
+  likeButton.textContent = 'Like';
+  likeButton.classList.add('like-button');
+
+  const commentButton = document.createElement('button');
+  commentButton.textContent = 'Comment';
+  commentButton.classList.add('comment-button');
+
+  const likeCounter = document.createElement('span');
+  likeCounter.textContent = '0 likes';
+  likeCounter.classList.add('like-counter');
+
+  const counterDiv = document.createElement('div');
+  counterDiv.appendChild(likeCounter);
+
+  const commentSection = document.createElement('div');
+  commentSection.classList.add('comment-section');
+
+  const commentHeading = document.createElement('small');
+  commentHeading.textContent = 'Comment Section';
+  commentHeading.classList.add('comment-heading');
+
   
-    const likeButton = document.createElement('button');
-    likeButton.textContent = 'Like';
-    likeButton.classList.add('like-button');
-  
-    const dislikeButton = document.createElement('button');
-    dislikeButton.textContent = 'Dislike';
-    dislikeButton.classList.add('dislike-button');
-  
-    const likeCounter = document.createElement('span');
-    likeCounter.textContent = '0 likes';
-    likeCounter.classList.add('like-counter');
-  
-    const dislikeCounter = document.createElement('span');
-    dislikeCounter.textContent = '0 dislikes';
-    dislikeCounter.classList.add('dislike-counter');
-  
-    const counterDiv = document.createElement('div');
-    counterDiv.appendChild(likeCounter);
-    counterDiv.appendChild(dislikeCounter);
-  
-    postElement.appendChild(userPosted);
-    postElement.appendChild(postTextElement);
-    postElement.appendChild(likeButton);
-    postElement.appendChild(dislikeButton);
-    postElement.appendChild(counterDiv);
-  
-    postCard.appendChild(postElement);
-  
-    // Attach event listeners to like and dislike buttons
-    likeButton.addEventListener('click', () => {
-      const currentLikes = parseInt(likeCounter.textContent);
-      likeCounter.textContent = currentLikes + 1 + " likes";
-  
-      // TODO: Send updated post data to the server
+  postElement.appendChild(userPosted);
+  postElement.appendChild(postTextElement);
+  postElement.appendChild(likeButton);
+  postElement.appendChild(commentButton);
+  postElement.appendChild(counterDiv);
+  postElement.appendChild(commentSection);
+  commentSection.appendChild(commentHeading);
+
+  postCard.appendChild(postElement);
+
+  // Attach event listeners to like and dislike buttons
+  likeButton.addEventListener('click', () => {
+    const currentLikes = parseInt(likeCounter.textContent);
+    likeCounter.textContent = currentLikes + 1 + " likes";
+
+    // TODO: Send updated post data to the server
+  });
+
+  commentButton.addEventListener('click', () => {
+
+
+    const commentInput = document.createElement('input');
+    commentInput.type = 'text';
+    commentInput.placeholder = 'Write a comment...';
+    commentInput.classList.add('comment-input-bar');
+
+    const commentSubmitButton = document.createElement('button');
+    commentSubmitButton.textContent = 'Submit';
+    commentSubmitButton.classList.add('comment-submit-button');
+
+    const commentCancelButton = document.createElement('button');
+    commentCancelButton.textContent = 'Cancel';
+    commentCancelButton.classList.add('comment-cancel-button');
+
+    const commentForm = document.createElement('form');
+    commentForm.classList.add('comment-form');
+    commentForm.appendChild(commentInput);
+    commentForm.appendChild(commentSubmitButton);
+    commentForm.appendChild(commentCancelButton);
+ 
+    commentSection.appendChild(commentForm);
+
+    commentSubmitButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      const commentInputValue = commentInput.value.trim();
+
+      if (commentInputValue !== '') {
+        const comment = document.createElement('div');
+        comment.classList.add('comment');
+        comment.innerHTML = `<small>a user commented</small><br> ${commentInputValue}`;
+
+        commentSection.appendChild(comment);
+        commentForm.remove();
+
+        // TODO: Send new comment to the server to be stored in the database
+      }
     });
-  
-    dislikeButton.addEventListener('click', () => {
-      const currentDislikes = parseInt(dislikeCounter.textContent);
-      dislikeCounter.textContent = currentDislikes + 1 + " dislikes";
-  
-      // TODO: Send updated post data to the server
+
+    commentCancelButton.addEventListener('click', () => {
+      commentForm.remove();
     });
-  
-    return postCard;
-  }
+  });
+
+  return postCard;
+}
+
   
 // Function to handle new post submission
 function handleNewPost() {
